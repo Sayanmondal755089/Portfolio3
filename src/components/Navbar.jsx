aa38ba6ba7b9a9e87775e14bf19f1d602739f9e6
Sayan Mondal <sanynmandal@gmail.com>
2025-12-05 22:43:11 +0530

    Change initials from 'KD' to 'SM' in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -64,7 +64,7 @@ export default function Navbar() {
               color: "var(--accent)",
             }}
           >
-            KD
+            SM
           </motion.div>
           <div style={{ display: "flex", flexDirection: "column" }}>
             <h1 style={{ margin: 0, fontSize: 14 }}>Sayan Mondal</h1>
