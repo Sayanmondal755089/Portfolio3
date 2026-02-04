c463e0eba2e895fd0a17d2c251eb2df803e06c9b
Sayan Mondal <sanynmandal@gmail.com>
2026-02-04 19:27:40 +0530

    Add SemesterTimeline component for academic journey

---
+++ src/pages/SemesterTimeline.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import "./SemesterTimeline.css";
+
+const SemesterTimeline = () => {
+  const semesters = [
+    "1st Semester (Done)",
+    "2nd Semester (Ongoing)",
+    "3rd Semester",
+    "4th Semester",
+    "5th Semester",
+    "6th Semester",
+    "7th Semester",
+    "8th Semester",
+  ];
+
+  return (
+    <section className="semester-section">
+      <h2 className="semester-title">Academic Journey</h2>
+
+      <div className="semester-container">
+        {semesters.map((sem, index) => (
+          <div
+            key={index}
+            className="semester-card"
+            style={{ animationDelay: `${index * 0.4}s` }}
+          >
+            {sem}
+          </div>
+        ))}
+      </div>
+    </section>
+  );
+};
+
+export default SemesterTimeline;
