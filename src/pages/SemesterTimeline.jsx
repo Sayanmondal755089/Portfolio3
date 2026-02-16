adae1009e7d0f274234264e69f4b3573d60575cb
Sayan Mondal <sanynmandal@gmail.com>
2026-02-16 16:59:02 +0530

    Fix formatting of 1st Semester label

--- src/pages/SemesterTimeline.jsx
+++ src/pages/SemesterTimeline.jsx
@@ -3,7 +3,8 @@ import "./SemesterTimeline.css";
 
 const SemesterTimeline = () => {
   const semesters = [
-    "1st Semester (Done)✅",
+    "1st Semester 
+       (Done)✅",
     "2nd Semester (Ongoing)",
     "3rd Semester",
     "4th Semester",
